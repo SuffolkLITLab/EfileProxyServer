614f5124c8b015d203981388f3ede74b2d40b0bb
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-06-24 23:42:56 -0400

    Added a class that handles the LIST taxonomy

---
+++ src/main/java/edu/suffolk/assemblyline/efspserver/LegalIssuesTaxonomyCodes.java
@@ -0,0 +1,132 @@
+package edu.suffolk.assemblyline.efspserver;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class LegalIssuesTaxonomyCodes {
+  private static Logger log = Logger.getLogger("edu.suffolk.assemblyline.efspserver.Person"); 
+
+  /** Reads a special version of the csv at originally from https://taxonomy.legal/download. 
+   * A column is added before "Taxonomies" that maps top level codes to ECF case types:
+   * https://docs.oasis-open.org/legalxml-courtfiling/specs/ecf/v4.01/ecf-v4.01-spec/errata02/os/ecf-v4.01-spec-errata02-os-complete.html#_Toc425241622
+   */
+  public LegalIssuesTaxonomyCodes(InputStream csvFile) throws IOException, CsvValidationException {
+    taxonomyGraph = new HashMap<String, InternalNode>();
+    Reader r = new InputStreamReader(csvFile); 
+    CSVReader reader = new CSVReader(r);
+    reader.skip(1); // The first line is the headers
+    String[] row = reader.readNext();
+    while (row != null) {
+      if (row.length < 5) {
+        reader.close();
+        throw new CsvValidationException();
+      }
+      Set<String> parents = new HashSet<String>(Arrays.asList(row[3].split(",")));
+      InternalNode node = new InternalNode(row[0], row[1], row[2], parents, 
+          (row[4].isBlank()) ? Optional.empty() : Optional.of(row[4]));
+      taxonomyGraph.put(row[0], node);
+      row = reader.readNext();
+    }
+    reader.close();
+    
+    this.calculateChildren();
+  }
+
+  public boolean contains(String code) {
+    return taxonomyGraph.containsKey(code);
+  }
+  
+  public Set<String> allEcfCaseTypes(Collection<String> categories) {
+    Set<String> ecfCases = new HashSet<String>();
+    for (String category : categories) {
+      getEcfCases(category, ecfCases);
+    }
+    return ecfCases;
+  }
+  
+  
+  Map<String, InternalNode> taxonomyGraph;
+  
+  private void calculateChildren() {
+    for (Map.Entry<String, InternalNode> entry : taxonomyGraph.entrySet()) {
+      for (Map.Entry<String, InternalNode> possibleParent : taxonomyGraph.entrySet()) {
+        if (possibleParent.getKey().equals(entry.getKey())) {
+          continue;
+        }
+        if (entry.getValue().parentIs(possibleParent.getKey())) {
+          possibleParent.getValue().addChild(entry.getKey());
+        }
+      }
+    }
+  }
+  
+  private void getEcfCases(String category, Set<String> runningCases) {
+    log.info("Trying key: " + category);
+    if (!taxonomyGraph.containsKey(category)) {
+      return; 
+    }
+
+    InternalNode node = taxonomyGraph.get(category);
+    log.info("At node: " + node.title);
+    if (node.caseCategory.isPresent()) {
+      log.info("Adding " + node.caseCategory.get() + " to running");
+      runningCases.add(node.caseCategory.get());
+      // TODO(brycew): should we exit early and not add the ECF case types from parent?
+      // Example: Money is usually Civil, but the Bankruptcy sub cat is just Bankruptcy
+      return;
+    }
+    for (String parent : node.parents) {
+      getEcfCases(parent, runningCases);
+    }
+    return; 
+  }
+
+  
+  /** A very lazy list-based adjacency directed graph implementation. 
+   * Each node has parents, and children. All nodes are contained in a map
+   * in the parent class.
+   */
+  class InternalNode {
+    String code;
+    String title;
+    String definition;
+    Optional<String> caseCategory;
+    
+    Set<String> parents;
+    Set<String> children;
+    
+    public InternalNode(String code, String title, String definition, 
+        Set<String> parents, Optional<String> caseType) {
+      this.code = code;
+      this.title = title;
+      this.definition = definition;
+      this.parents = parents;
+      this.children = new HashSet<String>();
+      this.caseCategory = caseType;
+    }
+    
+    public boolean parentIs(String parent) {
+      return parents.contains(parent);
+    }
+    
+    public boolean childIs(String child) {
+      return children.contains(child);
+    }
+    
+    public void addChild(String child) {
+      this.children.add(child);
+    }
+  }
+}
---
+++ src/test/java/edu/suffolk/assemblyline/efspserver/LegalIssuesTaxonomyCodesTest.java
@@ -0,0 +1,44 @@
+package edu.suffolk.assemblyline.efspserver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LegalIssuesTaxonomyCodesTest {
+
+  LegalIssuesTaxonomyCodes codes;
+
+  @BeforeEach
+  public void setUp() throws CsvValidationException, IOException {
+  }
+  
+  @Test
+  public void testThrowsOnBadFile() {
+    assertThrows(NullPointerException.class, () -> new LegalIssuesTaxonomyCodes(null));  
+    assertThrows(CsvValidationException.class, () -> {
+      new LegalIssuesTaxonomyCodes(this.getClass().getResourceAsStream(
+          "/housing_tro_2_plaintiff_business_def_no_email.json"));
+    });
+  }
+  
+  @Test
+  public void testCorrectEcfMappings() throws CsvValidationException, IOException {
+    codes = new LegalIssuesTaxonomyCodes(this.getClass().getResourceAsStream("/taxonomy.csv"));
+    Set<String> bankruptcy = codes.allEcfCaseTypes(List.of("MO-13-00-00-00"));
+    assertEquals(Set.of("BankruptcyCase"), bankruptcy);
+    
+    Set<String> domestic = codes.allEcfCaseTypes(List.of("MO-00-00-00-00", "HE-00-00-00-00"));
+    assertEquals(Set.of("CivilCase"), domestic);
+   
+    Set<String> domesticAndCivil = 
+        codes.allEcfCaseTypes(List.of("FA-06-00-00-00", "TO-04-00-00-00"));
+    assertEquals(Set.of("DomesticCase", "CivilCase"), domesticAndCivil);
+  }
+
+}
