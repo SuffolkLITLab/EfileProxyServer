5547079536ce805d528129f38272a3990d7f73ba
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-07-01 11:48:50 -0400

    Add shell for SendMessage and import in new endpoint

---
+++ src/main/java/edu/suffolk/litlab/efspserver/SendMessage.java
@@ -0,0 +1,17 @@
+package edu.suffolk.litlab.efspserver;
+
+import com.twilio.*;
+import com.sendgrid.*;
+
+
+public class SendMessage {
+  
+  // SendSMS
+  // SendEmail
+  // private SendTwilioSMS
+  // private SendSendgridEmail
+
+  // in EFSPServer.java, we load various configurations. That would choose the
+  // parameters that are set in the classes. No overloading of the method parameters--always require the type
+
+}
--- src/main/java/edu/suffolk/litlab/efspserver/services/EfmCallback.java
+++ src/main/java/edu/suffolk/litlab/efspserver/services/EfmCallback.java
@@ -1,5 +1,33 @@
 package edu.suffolk.litlab.efspserver.services;
 
+import edu.suffolk.litlab.efspserver.SendMessage;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+
+/*
+ * This is just for custom endpoints, like Jefferson Parish. Other callbacks will be a SOAP server.
+*/
+
+@Path("/status_callback/")
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class EfmCallback {
-  
+  private static Logger log = 
+      Logger.getLogger("edu.suffolk.litlab.efspserver.services.EfmCallback"); 
+
+  // @POST("/court/{court_id}/{filing_id}/status")
+  /*
+  public Response getFilingStatus(@Context HttpHeaders httpHeaders,
+  @PathParam("court_id") String courtId, 
+  @PathParam("filing_id") String filingId)
+  // the JP callback will include the same authentication token that we sent them
+  */
 }
+
+// create endpoints with annotations like @GET
+// comes from JAXRS
