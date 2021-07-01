482a2acaeb8239bdc3a8cbec6ce439db18280e3c
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-07-01 11:03:55 -0400

    Actually rename uses of assemblyline to litlab

--- docker_run_script.sh
+++ docker_run_script.sh
@@ -2,5 +2,5 @@
 
 set -ex
 cd /usr/src/app
-java -cp $(cat cp.txt):target/assemblyline-efsp-0.0.1-SNAPSHOT.jar edu.suffolk.assemblyline.efspserver.services.EfspServer
+java -cp $(cat cp.txt):target/assemblyline-efsp-0.0.1-SNAPSHOT.jar edu.suffolk.litlab.efspserver.services.EfspServer
 # TODO(brycew): if needed to debug http logs: https://stackoverflow.com/questions/53215038/how-to-log-request-response-using-java-net-http-httpclient
