497bc63c7fe0645573ec5260b8a3fd2e7ac28cc6
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-07-10 14:57:30 -0400

    Cleaned up MVP a bit, just with checkstyle

--- src/main/java/edu/suffolk/litlab/efspserver/SendMessage.java
+++ src/main/java/edu/suffolk/litlab/efspserver/SendMessage.java
@@ -1,44 +1,51 @@
 package edu.suffolk.litlab.efspserver;
 
-import java.io.IOException;
-import java.util.HashMap;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-import com.sendgrid.*;
+import com.hubspot.jinjava.Jinjava;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-import com.hubspot.jinjava.Jinjava;
-
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class SendMessage {
 
+  /** Main just for testing. */
   public static void main(String[] args) throws IOException {
-    HashMap<String, Object> context = new HashMap();
+    HashMap<String, Object> context = new HashMap<String, Object>();
     context.put("name", "Suffolk LIT Lab Test");
-    // SendEmail("test@example.com", "Test email", "massaccess@suffolk.edu", "Hello, {{ name }}. message body", context);
-
-    //SendSMS("+1.........", "Hello {{ name }}", context);
+    // sendEmail("test@example.com", "Test email", "massaccess@suffolk.edu", 
+    //    "Hello, {{ name }}. message body", context);
+    //sendSms("+1.........", "Hello {{ name }}", context);
   }
   
   /**
-   * Send an email, currently using the SendGrid API. Depends on the environment variable SENDGRID_API_KEY
+   * Send an email, currently using the SendGrid API. Depends on the env variable SENDGRID_API_KEY.
    * which should be a valid API key for Twilio SendGrid.
+   *
    * @param from The email "from" address
    * @param subject The email subject
    * @param to The email recipient
-   * @param message_template A string with optional Jinja2 template variables. Will be rendered as plain text.
+   * @param messageTemplate A string with optional Jinja2 template variables.
+   *        Will be rendered as plain text.
    * @param context A HashMap containing variables that will be substituted in the message_template
-   * @throws IOException
+   * @throws IOException If there's a network error talking to sendgrid
    */
-  public static Integer SendEmail(String from, String subject, String to, String message_template, HashMap<String, Object> context) throws IOException {
+  public static Integer sendEmail(String from, String subject, String to, 
+      String messageTemplate, HashMap<String, Object> context) throws IOException {
     SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
     Request request = new Request();
     Jinjava jinjava = new Jinjava();
-    String rendered_template = jinjava.render(message_template, context);
+    String renderedTemplate = jinjava.render(messageTemplate, context);
 
-    Mail mail = new Mail(new Email(from), subject, new Email(to), new Content("text/plain", rendered_template) );
+    Mail mail = new Mail(new Email(from), subject, new Email(to), 
+        new Content("text/plain", renderedTemplate));
 
     try {
       request.setMethod(Method.POST);
@@ -55,23 +62,26 @@ public class SendMessage {
   }
 
   /**
-   * Send an SMS Message, currently using Twilio SMS API. Depends on environment variables TWILIO_ACCOUNT_SID,
-   * TWILIO_AUTH_TOKEN, and TWILIO_SENDING_NUMBER. 
+   * Send an SMS Message, currently using Twilio SMS API. Depends on env variables 
+   * - TWILIO_ACCOUNT_SID.
+   * - TWILIO_AUTH_TOKEN, and 
+   * - TWILIO_SENDING_NUMBER. 
+   *
    * @param to a valid phone number for SMS messaging, in ISO format.
-   * @param message_template A string with Jinja2 template variables. Will be rendered as plain text.
+   * @param messageTemplate A string with Jinja2 template variables. 
+   *        Will be rendered as plain text.
    * @param context a HashMap with variables that will be substituted into the message_template
-   * @throws IOException
    */
-  public static String SendSMS(String to, String message_template, HashMap<String, Object> context) throws IOException {
-
+  public static String sendSms(String to, 
+      String messageTemplate, HashMap<String, Object> context) {
     Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN"));
     String from = System.getenv("TWILIO_SENDING_NUMBER");
     Jinjava jinjava = new Jinjava();
-    String rendered_template = jinjava.render(message_template, context);
+    String renderedTemplate = jinjava.render(messageTemplate, context);
 
     Message message = Message.creator(new PhoneNumber(to), 
         new PhoneNumber(from),
-        rendered_template).create();
+        renderedTemplate).create();
     
     return message.getSid();
   }
@@ -83,12 +93,12 @@ public class SendMessage {
   public static boolean isValidPhoneNumber() {
     return true;
   }
-  // SendSMS
-  // SendEmail
+  // TODO: maybe allow multiple services?
   // private SendTwilioSMS
   // private SendSendgridEmail
 
   // in EFSPServer.java, we load various configurations. That would choose the
-  // parameters that are set in the classes. No overloading of the method parameters--always require the type
+  // parameters that are set in the classes. 
+  // No overloading of the method parameters--always require the type
 
 }
