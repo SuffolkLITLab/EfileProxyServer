96cb358dcf93501091760632cb3b0a58fa097298
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-12-03 16:49:13 -0500

    Added services

---
+++ src/test/java/edu/suffolk/litlab/efspserver/services/Ecfv5XmlHelper.java
@@ -0,0 +1,14 @@
+package edu.suffolk.litlab.efspserver.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Ecfv5XmlHelper {
+
+  @Test
+  public void testCurrentTimeStr() {
+    assertEquals(0, 0);
+  }
+  
+}
