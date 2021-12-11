43921686bceacd3b4e29b15f22df3e1102b1e628
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-12-10 22:37:33 -0500

    Fix for Log4Shell vuln
    We shouldn't be vulnerable, since we use the jdk logger and slf4j,
    not log4j. And from what I can tell none of the libraries that we use
    have log4j as a dependency, but it's hard to tell. So might as well patch.

--- docker_run_script.sh
+++ docker_run_script.sh
@@ -4,5 +4,6 @@ set -ex
 cd /usr/src/app
 java -cp $(cat cp.txt):target/assemblyline-efsp-0.3.3.jar \
 	edu.suffolk.litlab.efspserver.services.EfspServer \
-	-Djava.util.logging.config.file=logging.properties
+	-Djava.util.logging.config.file=logging.properties \
+    -Dlog4j2.formatMsgNoLookups=true
 # if needed to debug http logs: https://stackoverflow.com/questions/53215038/how-to-log-request-response-using-java-net-http-httpclient
