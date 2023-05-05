21a0250ce4389819a72a36987eade18937b66037
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-05-05 11:59:26 -0400

    Actually, we don't use POST / PUT / DELETE for the codes

--- src/main/java/edu/suffolk/litlab/efspserver/ecf/EcfCodesService.java
+++ src/main/java/edu/suffolk/litlab/efspserver/ecf/EcfCodesService.java
@@ -652,7 +652,7 @@ public class EcfCodesService implements CodesService {
         .header("Access-Control-Allow-Origin", "*")
         .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
         .header("Access-Control-Allow-Credentials", "true")
-        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        .header("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD")
         .header("Access-Control-Max-Age", "1209600")
         .build();
   }
