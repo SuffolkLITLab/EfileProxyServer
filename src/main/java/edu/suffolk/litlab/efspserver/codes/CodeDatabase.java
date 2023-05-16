01d13aefb0b5f64f8b0088a28a0b9bc0f04d9122
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-05-16 16:48:48 -0400

    Removed optionalservices from table list, not used anymore

--- src/main/java/edu/suffolk/litlab/efspserver/codes/CodeDatabase.java
+++ src/main/java/edu/suffolk/litlab/efspserver/codes/CodeDatabase.java
@@ -83,7 +83,6 @@ public class CodeDatabase implements DatabaseInterface, AutoCloseable {
   }
 
   public void createTableIfAbsent(String tableName) throws SQLException {
-    log.info("Start of creatTableIfAbsent: {}", tableName);
     if (conn == null) {
       throw new SQLException();
     }
