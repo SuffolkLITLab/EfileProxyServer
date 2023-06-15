SuffolkLITLab/EfileProxyServer

commit 57f69480e22fe397421dd2b54477abb8e8f13c9b

Smaller codes DB tables (#138)
Shrinks the code tables, particularly the optional services one, which required some refactoring to not assume that most operations take only a single transaction.

It shrinks the disk usage by a few GB, can't find my notes that I have for how much. It wasn't a ton though, and would need to go to enums if we wanted to actually decrease usage.

For now though, addresses #99.

* Update 6to7, smaller database than otherwise

More progress; still fails test from nothing

Had to pivot again, and start putting a lot of specific DELETE, INSERT,
and CREATE operations into the optional service class, since
that table is now two tables, which means two statements, which breaks all
of the abstractions we had that adding to a single codes table is adding to
a single SQL table. Just special cased a lot of stuff for Optionalservices.

Passed all tests, still need to local test

* Removed optionalservices from table list, not used anymore
* DataFieldRow neews booleans now, not strings
* Fix tests for optsrv, add test for deleting
* Return after deleting from optionalservices

Instead of continuing to non-optional service stuff.

src/main/java/edu/suffolk/litlab/efspserver/codes/CodeTableConstants.java
@@ -17,7 +17,7 @@ public class CodeTableConstants {
   private static final Map<String, String> createQueries = new HashMap<>();
   private static final Map<String, String> insertQueries = new HashMap<>();
   private static final Map<String, String> deleteFromQueries = new HashMap<>();
-  private static final Map<String, String> deleteAllJurisdictionFromQueries = new HashMap<>();
+  private static final Map<String, String> deleteAllCourtsFromQueries = new HashMap<>();
 
   static {
     List<Pair<String, String>> locationColumns = new ArrayList<Pair<String, String>>();
@@ -93,17 +93,17 @@ public class CodeTableConstants {
           new ImmutablePair<String, String>("code", "text"),
           new ImmutablePair<String, String>("name", "text")))),
         Map.entry("datafieldconfig", makeCourtColumnInfo(List.of(
-          new ImmutablePair<String, String>("code", "text"),
+          new ImmutablePair<String, String>("code", "varchar(40)"),
           new ImmutablePair<String, String>("name", "text"), 
-          new ImmutablePair<String, String>("isvisible", "text"),
-          new ImmutablePair<String, String>("isrequired", "text"), 
+          new ImmutablePair<String, String>("isvisible", "boolean"),
+          new ImmutablePair<String, String>("isrequired", "boolean"), 
           new ImmutablePair<String, String>("helptext", "text"),
           new ImmutablePair<String, String>("ghosttext", "text"),
           new ImmutablePair<String, String>("contextualhelpdata", "text"),
           new ImmutablePair<String, String>("validationmessage", "text"),
           new ImmutablePair<String, String>("regularexpression", "text"),
           new ImmutablePair<String, String>("defaultvalueexpression", "text"),
-          new ImmutablePair<String, String>("isreadonly", "text")))),
+          new ImmutablePair<String, String>("isreadonly", "boolean")))),
         ///////// Tables for courts specifically
         Map.entry("answer", makeCourtColumnInfo(List.of(
           new ImmutablePair<String, String>("code", "text"),
@@ -134,7 +134,7 @@ public class CodeTableConstants {
           new ImmutablePair<String, String>("casetypeid", "text"),
           new ImmutablePair<String, String>("efspcode", "text")))),
         Map.entry("casetype", makeCourtColumnInfo(List.of(
-          new ImmutablePair<String, String>("code", "text"),
+          new ImmutablePair<String, String>("code", "varchar(40)"),
           new ImmutablePair<String, String>("name", "text"),
           new ImmutablePair<String, String>("casecategory", "text"),
           new ImmutablePair<String, String>("initial", "text"),
@@ -204,26 +204,26 @@ public class CodeTableConstants {
           new ImmutablePair<String, String>("extension", "text"),
           new ImmutablePair<String, String>("efspcode", "text")))),
         Map.entry("filing", makeCourtColumnInfo(List.of(
-          new ImmutablePair<String, String>("code", "text"), 
+          new ImmutablePair<String, String>("code", "varchar(40)"), 
           new ImmutablePair<String, String>("name", "text"),
-          new ImmutablePair<String, String>("fee", "text"), 
+          new ImmutablePair<String, String>("fee", "text"),
           new ImmutablePair<String, String>("casecategory", "text"),
           new ImmutablePair<String, String>("casetypeid", "text"),
           new ImmutablePair<String, String>("filingtype", "text"),
-          new ImmutablePair<String, String>("iscourtuseonly", "text"),
+          new ImmutablePair<String, String>("iscourtuseonly", "boolean"),
           new ImmutablePair<String, String>("civilclaimamount", "text"),
           new ImmutablePair<String, String>("probateestateamount", "text"),
           new ImmutablePair<String, String>("amountincontroversy", "text"),
-          new ImmutablePair<String, String>("useduedate", "text"),
-          new ImmutablePair<String, String>("isproposedorder", "text"),
+          new ImmutablePair<String, String>("useduedate", "boolean"),
+          new ImmutablePair<String, String>("isproposedorder", "boolean"),
           new ImmutablePair<String, String>("efspcode", "text")))),
         Map.entry("filingcomponent", makeCourtColumnInfo(List.of(
-          new ImmutablePair<>("code", "text"), 
+          new ImmutablePair<>("code", "varchar(40)"), 
           new ImmutablePair<>("name", "text"),
-          new ImmutablePair<>("filingcodeid", "text"),
-          new ImmutablePair<>("required", "text"),
-          new ImmutablePair<>("allowmultiple", "text"),
-          new ImmutablePair<>("displayorder", "text"),
+          new ImmutablePair<>("filingcodeid", "varchar(40)"),
+          new ImmutablePair<>("required", "boolean"),
+          new ImmutablePair<>("allowmultiple", "boolean"),
+          new ImmutablePair<>("displayorder", "integer"),
           new ImmutablePair<>("efspcode", "text")))),
         Map.entry("generaloffense", makeCourtColumnInfo(List.of(
           new ImmutablePair<>("code", "text"), 
@@ -251,23 +251,12 @@ public class CodeTableConstants {
           new ImmutablePair<String, String>("code", "text"),
           new ImmutablePair<String, String>("name", "text"), 
           new ImmutablePair<String, String>("efspcode", "text")))),
-        Map.entry("optionalservices", makeCourtColumnInfo(List.of(
-          new ImmutablePair<String, String>("code", "text"), 
-          new ImmutablePair<String, String>("name", "text"),
-          new ImmutablePair<String, String>("displayorder", "text"), 
-          new ImmutablePair<String, String>("fee", "text"),
-          new ImmutablePair<String, String>("filingcodeid", "text"),
-          new ImmutablePair<String, String>("multiplier", "text"),
-          new ImmutablePair<String, String>("altfeedesc", "text"),
-          new ImmutablePair<String, String>("hasfeeprompt", "text"),
-          new ImmutablePair<String, String>("feeprompttext", "text"),
-          new ImmutablePair<String, String>("efspcode", "text")))),
         Map.entry("partytype", makeCourtColumnInfo(List.of(
-          new ImmutablePair<String, String>("code", "text"), 
+          new ImmutablePair<String, String>("code", "varchar(40)"), 
           new ImmutablePair<String, String>("name", "text"),
-          new ImmutablePair<String, String>("isavailablefornewparties", "text"),
+          new ImmutablePair<String, String>("isavailablefornewparties", "boolean"),
           new ImmutablePair<String, String>("casetypeid", "text"),
-          new ImmutablePair<String, String>("isrequired", "text"),
+          new ImmutablePair<String, String>("isrequired", "boolean"),
           new ImmutablePair<String, String>("amount", "text"),
           new ImmutablePair<String, String>("numberofpartiestoignore", "text"),
           new ImmutablePair<String, String>("sendforredaction", "text"),
@@ -330,22 +319,21 @@ public class CodeTableConstants {
     for (Map.Entry<String, TableColumns> table : tableColumns.entrySet()) {
       createQueries.put(table.getKey(), createTableQuery(table.getKey(), table.getValue()));
       insertQueries.put(table.getKey(), createInsertQuery(table.getKey(), table.getValue()));
-      deleteAllJurisdictionFromQueries.put(table.getKey(), "DELETE FROM " + table.getKey() + " WHERE domain=?");
+      deleteAllCourtsFromQueries.put(table.getKey(), "DELETE FROM " + table.getKey() + " WHERE domain=?");
       if (table.getValue().needsExtraLocCol) {
         deleteFromQueries.put(table.getKey(), "DELETE FROM " + table.getKey() + " WHERE domain=? AND location=?");
       }
     }
     
-    tableIndices.put("optionalservices", List.of(
-        "CREATE INDEX ON optionalservices (location)",
-        "CREATE INDEX ON optionalservices (filingcodeid)"));
     tableIndices.put("filing", List.of(
         "CREATE INDEX ON filing (location)",
         "CREATE INDEX ON filing (casecategory)",
-        "CREATE INDEX ON filing (casetypeid)")); 
+        "CREATE INDEX ON filing (casetypeid)",
+        "CREATE INDEX ON filing (domain)"));
     tableIndices.put("filingcomponent", List.of(
         "CREATE INDEX ON filingcomponent (location)", 
-        "CREATE INDEX ON filingcomponent (filingcodeid)")); 
+        "CREATE INDEX ON filingcomponent (filingcodeid)",
+        "CREATE INDEX ON filingcomponent (domain)")); 
   }
 
   public static String getZipNameFromTable(String tableName) {
@@ -388,6 +376,10 @@ public class CodeTableConstants {
     // Drops the type in the column name + type pairs.
     return tableColumns.getOrDefault(tableName, new TableColumns())
         .mainList.stream().map((e) -> e.getLeft()).collect(Collectors.toList());
+  }
+
+  public static List<Pair<String, String>> getTableColumnsWithType(String tableName) {
+    return tableColumns.getOrDefault(tableName, new TableColumns()).mainList;
   }
   
   public static boolean isCourtTable(String tableName) {
@@ -491,11 +483,11 @@ public class CodeTableConstants {
     return deleteFromQueries.get(tableName);
   }
 
-  public static String getDeleteAllJurisdictionFrom(String tableName) {
-    if (!deleteAllJurisdictionFromQueries.containsKey(tableName)) {
+  public static String getDeleteAllCourtsFrom(String tableName) {
+    if (!deleteAllCourtsFromQueries.containsKey(tableName)) {
       return "";
     }
-    return deleteAllJurisdictionFromQueries.get(tableName);
+    return deleteAllCourtsFromQueries.get(tableName);
   }
 
   /**
@@ -551,9 +543,9 @@ public class CodeTableConstants {
       createLocation.append("\"" + col.getLeft() + "\" " + col.getRight());
     }
     if (tc.needsExtraLocCol) {
-      createLocation.append(", \"location\" text");
+      createLocation.append(", \"location\" varchar(80)");
     }
-    createLocation.append(", \"domain\" text");
+    createLocation.append(", \"domain\" varchar(80)");
 
     if (!tc.primaryKeys.isEmpty()) {
       createLocation.append(

commit e950de741b8aa324f08e205ce8e97a30cda6370d

Integration test revamp (#139)
Added an easy script to run integration tests front to back, and slight improvements based on coverage data.

Previously, you would have to start up an the server separately, likely with a fully set-up database (that takes ~30-40 minutes for just one jurisdiction).

Now, there's a separate integration test that can generate coverage data for the java server code. It starts up by only downloading codes for one location to speed up start up time, and runs with the integration test script in EFSPIntegration.

There are also some fixes based on these integration tests run:

* Changed datafieldconfig column code to varchar 80. Bug introduced in #138, this only affects new databases made from scratch, which haven't happened yet. So this doesn't need to be in the version update.
* login database server key generation should create the tables if they don't exist
* HATEOS fixes:
    * add getDataFields to the ECFCodesService HATEOS
    * AdminUserServers had an extra /` on the end of HATEOS URLs, which causes issues on the current jakarta server
    * removes endpoints that don't exist based on the jurisdiction (i.e. Louisiana not having all endpoints)
* if there aren't message settings for a server, return the default settings, not 404
* removed the internal test trigger for when the Tyler EFM returns certain error codes

.gitignore
@@ -1,5 +1,12 @@
 target/
 
+# stuff related to running integration tests
+/cp.txt
+/proxy_stuff.txt
+/jacoco.exec
+jacoco-0.8.10/
+
+
 # Compiled class file
 *.class
 *.swp

Dockerfile
@@ -1,14 +1,12 @@
 FROM maven:3.8-eclipse-temurin-17-alpine
 # The `[]` is an optional COPY: doesn't copy if those files aren't there (https://stackoverflow.com/a/46801962/11416267)
 # They are needed for Tyler API usage, and serving the REST API as HTTPS
-COPY pom.xml LICENSE client_sign.propertie[s] quartz.properties Suffolk.pf[x] acme_user.ke[y] acme_domain.ke[y] acme_domain-chain.cr[t] extract-tls-secrets-4.0.0.ja[r] /usr/src/app/
+COPY pom.xml LICENSE client_sign.propertie[s] quartz.properties Suffolk.pf[x] acme_user.ke[y] acme_domain.ke[y] acme_domain-chain.cr[t] extract-tls-secrets-4.0.0.ja[r] jacocoagent.ja[r] /usr/src/app/
 # Install all of the maven packages, so we don't have to every time we change code
 RUN mvn -f /usr/src/app/pom.xml -DskipTests clean dependency:resolve dependency:go-offline package && mvn -f /usr/src/app/pom.xml test
 COPY src /usr/src/app/src
 RUN mvn -f /usr/src/app/pom.xml -DskipTests package dependency:build-classpath -Dmdep.outputFile=cp.txt -PnoDockerTests
-COPY docker_run_script.sh /usr/src/app/
-# Use if you need to debug the exact SOAP messages being sent
-# COPY extract-tls-secrets-4.0.0.jar /usr/src/app/
+COPY docker_run_script.sh docker_integration_test.sh /usr/src/app/
 
 EXPOSE 9000
 

pom.xml
@@ -434,7 +434,7 @@
                 <plugin>
                     <groupId>org.jacoco</groupId>
                     <artifactId>jacoco-maven-plugin</artifactId>
-                    <version>0.8.7</version>
+                    <version>0.8.10</version>
                     <configuration>
                         <excludes>
                             <exclude>ecfv5/**/*.class</exclude>

src/main/java/edu/suffolk/litlab/efspserver/codes/CodeTableConstants.java
@@ -93,7 +93,7 @@ public class CodeTableConstants {
           new ImmutablePair<String, String>("code", "text"),
           new ImmutablePair<String, String>("name", "text")))),
         Map.entry("datafieldconfig", makeCourtColumnInfo(List.of(
-          new ImmutablePair<String, String>("code", "varchar(40)"),
+          new ImmutablePair<String, String>("code", "varchar(80)"),
           new ImmutablePair<String, String>("name", "text"), 
           new ImmutablePair<String, String>("isvisible", "boolean"),
           new ImmutablePair<String, String>("isrequired", "boolean"), 

src/main/java/edu/suffolk/litlab/efspserver/codes/CodeUpdater.java
@@ -598,7 +598,7 @@ public class CodeUpdater {
       if (command.equalsIgnoreCase("replaceall")) {
         return cu.replaceAll(codesSite, filingPort, cd);
       } else if (command.equalsIgnoreCase("replacesome")) {
-        return cu.replaceSome(codesSite, filingPort, cd, args);
+        return cu.replaceSome(codesSite, filingPort, cd, args.subList(1, args.size()));
       } else if (command.equalsIgnoreCase("refresh")) {
         return cu.updateAll(codesSite, filingPort, cd);
       } else if (command.equalsIgnoreCase("downloadIndiv")) {

src/main/java/edu/suffolk/litlab/efspserver/db/LoginDatabase.java
@@ -293,6 +293,7 @@ public class LoginDatabase implements DatabaseInterface {
       LoginDatabase ld = new LoginDatabase(conn);
       boolean tylerBool = Boolean.parseBoolean(tylerEnabled);
       boolean jeffnetBool = Boolean.parseBoolean(jeffnetEnabled);
+      ld.createTablesIfAbsent();
       String newApiKey = ld.addNewUser(serverName, tylerBool, jeffnetBool);
       System.out.println("New Api Key for " + serverName + ": " + newApiKey);
       System.out.println("Using tyler: " + tylerBool + " and using jeffnet: " + jeffnetBool);

src/main/java/edu/suffolk/litlab/efspserver/ecf/EcfCodesService.java
@@ -54,6 +54,7 @@ public class EcfCodesService implements CodesService {
           "getCaseTypes",
           "getNameSuffixes",
           "getServiceTypes",
+          "getDataFields",
           "getProcedureOrRemedies",
           "getFilingTypes",
           "getDamageAmounts",

src/main/java/edu/suffolk/litlab/efspserver/ecf/TylerModuleSetup.java
@@ -164,9 +164,25 @@ public class TylerModuleSetup implements EfmModuleSetup {
           "All locations for " + this.tylerJurisdiction + "-" + this.tylerEnv + ": " + locations);
       boolean downloadAll = (cd.getAllLocations().size() == 0);
       if (downloadAll) {
-        log.info("Downloading all codes for " + tylerJurisdiction + ": please wait a bit");
-        CodeUpdater.executeCommand(
-            cd, tylerJurisdiction, tylerEnv, List.of("replaceall"), this.x509Password);
+        String testOnlyLocation = StdLib.GetEnv("_TEST_ONLY_LOCATION").orElse("");
+        if (!testOnlyLocation.isBlank()) {
+          log.info(
+              "Downloading just codes for "
+                  + testOnlyLocation
+                  + " in "
+                  + tylerJurisdiction
+                  + ": please wait a bit");
+          CodeUpdater.executeCommand(
+              cd,
+              tylerJurisdiction,
+              tylerEnv,
+              List.of("replacesome", testOnlyLocation),
+              this.x509Password);
+        } else {
+          log.info("Downloading all codes for " + tylerJurisdiction + ": please wait a bit");
+          CodeUpdater.executeCommand(
+              cd, tylerJurisdiction, tylerEnv, List.of("replaceall"), this.x509Password);
+        }
       }
     } catch (SQLException e) {
       log.error("SQL Exception: " + StdLib.strFromException(e));

src/main/java/edu/suffolk/litlab/efspserver/services/AdminUserService.java
@@ -115,7 +115,7 @@ public class AdminUserService {
   @Path("/")
   public Response getAll() {
     EndpointReflection ef =
-        new EndpointReflection("/jurisdictions/" + jurisdiction + "/adminusers/");
+        new EndpointReflection("/jurisdictions/" + jurisdiction + "/adminusers");
     return Response.ok(ef.endPointsToMap(ef.findRESTEndpoints(List.of(AdminUserService.class))))
         .build();
   }

src/main/java/edu/suffolk/litlab/efspserver/services/CasesService.java
@@ -43,7 +43,6 @@ import oasis.names.tc.legalxml_courtfiling.schema.xsd.caselistquerymessage_4.Cas
 import oasis.names.tc.legalxml_courtfiling.schema.xsd.caselistresponsemessage_4.CaseListResponseMessageType;
 import oasis.names.tc.legalxml_courtfiling.schema.xsd.casequerymessage_4.CaseQueryMessageType;
 import oasis.names.tc.legalxml_courtfiling.schema.xsd.caseresponsemessage_4.CaseResponseMessageType;
-import oasis.names.tc.legalxml_courtfiling.schema.xsd.commontypes_4.ErrorType;
 import oasis.names.tc.legalxml_courtfiling.schema.xsd.commontypes_4.OrganizationType;
 import oasis.names.tc.legalxml_courtfiling.schema.xsd.commontypes_4.PersonType;
 import oasis.names.tc.legalxml_courtfiling.schema.xsd.commontypes_4.QueryResponseMessageType;
@@ -171,15 +170,6 @@ public class CasesService {
       return Response.status(500).build();
     }
 
-    boolean internalTestTrigger = docketId != null && docketId.equals("abc123SecretTrigger");
-    if (internalTestTrigger) {
-      firstName = "John";
-      middleName = "";
-      lastName = "Brown";
-      businessName = null;
-      docketId = null;
-    }
-
     CaseListQueryMessageType query = new CaseListQueryMessageType();
     EntityType typ = new EntityType();
     JAXBElement<PersonType> elem2 = ecfOf.createEntityPerson(new PersonType());
@@ -221,12 +211,6 @@ public class CasesService {
 
     log.info("Before the case list query");
     CaseListResponseMessageType resp = maybePort.get().getCaseList(query);
-    if (internalTestTrigger) {
-      ErrorType et = new ErrorType();
-      et.setErrorCode(XmlHelper.convertText("-10"));
-      et.setErrorText(XmlHelper.convertText("Haha, secret code activated"));
-      resp.getError().add(et);
-    }
     log.info(XmlHelper.objectToXmlStrOrError(resp, CaseListResponseMessageType.class));
 
     MDCWrappers.removeAllMDCs();

src/main/java/edu/suffolk/litlab/efspserver/services/CourtSchedulingService.java
@@ -77,7 +77,6 @@ import tyler.ecf.v5_0.extensions.returndate.ReturnDateMessageType;
 import tyler.ecf.v5_0.extensions.returndateresponse.ReturnDateResponseMessageType;
 import tyler.efm.wsdl.webservicesprofile_implementation_4_0.CourtRecordMDEService;
 
-@Path("/scheduling/")
 @Produces({MediaType.APPLICATION_JSON})
 public class CourtSchedulingService {
 

src/main/java/edu/suffolk/litlab/efspserver/services/EfspServer.java
@@ -242,8 +242,10 @@ public class EfspServer {
             new Thread() {
               @Override
               public void run() {
+                log.info("Stopping servers");
                 server.stopServers();
                 for (EfmModuleSetup mod : modules) {
+                  log.info("Stopping module " + mod.getJurisdiction());
                   mod.shutdown();
                 }
               }

src/main/java/edu/suffolk/litlab/efspserver/services/JurisdictionServiceHandle.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
@@ -114,6 +115,22 @@ public class JurisdictionServiceHandle {
     var x = ef.findRESTEndpoints(List.of(JurisdictionServiceHandle.class));
     log.info("All endpoints for servicehandle: " + x);
     var map = ef.endPointsToMap(x);
+    ArrayList<String> keysToRemove = new ArrayList<>();
+    for (String key : map.keySet()) {
+      var val = map.get(key);
+      String url = val.get("url");
+      if ((url.endsWith("adminusers") && adminUser.isEmpty())
+          || (url.endsWith("codes") && codes.isEmpty())
+          || (url.endsWith("cases") && cases.isEmpty())
+          || (url.endsWith("scheduling") && courtScheduling.isEmpty())
+          || (url.endsWith("firmattorneyservice") && firmAttorneyAndService.isEmpty())
+          || (url.endsWith("payments") && payments.isEmpty())) {
+        keysToRemove.add(key);
+      }
+    }
+    for (String key : keysToRemove) {
+      map.remove(key);
+    }
     log.info("Hitting HATEOS endpoint for " + jurisdiction + ": map: " + map);
     return Response.ok(map).build();
   }

src/main/java/edu/suffolk/litlab/efspserver/services/MessageSettingsService.java
@@ -50,24 +50,20 @@ public class MessageSettingsService {
   @Path("/settings")
   public Response getMsgSettings(@Context HttpHeaders httpHeaders) {
     MDC.put(MDCWrappers.OPERATION, "MessageSettingsService.getMsgSettings");
+    OrgMessageSender orgMsg = new OrgMessageSender(ds, null);
     try (Connection conn = ds.getConnection()) {
       LoginDatabase ld = new LoginDatabase(conn);
       Optional<AtRest> atRest = ld.getAtRestInfo(httpHeaders.getHeaderString("X-API-KEY"));
       if (atRest.isEmpty()) {
         return Response.status(401).entity("\"Not logged in to efile\"").build();
       }
-      var md = new MessageSettingsDatabase(conn);
-      Optional<MessageInfo> info = md.findMessageInfo(atRest.get().serverId);
-      if (info.isEmpty()) {
-        MDCWrappers.removeAllMDCs();
-        return Response.status(404).entity("\"No current email settings for this server\"").build();
-      }
-      MDCWrappers.removeAllMDCs();
-      return Response.ok(info.get()).build();
+      MessageInfo info = orgMsg.getSettings(atRest.get().serverId);
+      return Response.ok(info).build();
     } catch (SQLException ex) {
       log.error("Couldn't get email settings for server: " + StdLib.strFromException(ex));
-      MDCWrappers.removeAllMDCs();
       return Response.status(500).build();
+    } finally {
+      MDCWrappers.removeAllMDCs();
     }
   }
 

src/main/java/edu/suffolk/litlab/efspserver/services/OrgMessageSender.java
@@ -76,7 +76,7 @@ public class OrgMessageSender {
     this.sendMsg = sendMsg;
   }
 
-  private MessageInfo getSettings(UUID serverId) {
+  public MessageInfo getSettings(UUID serverId) {
     Optional<MessageInfo> maybeInfo = Optional.empty();
     try (Connection conn = ds.getConnection()) {
       var md = new MessageSettingsDatabase(conn);

docker-integration-test.yml
@@ -1,0 +1,41 @@
+version: "3.9"
+
+services:
+  efspjava-test:
+    build:
+      context: .
+    # Necessary b/c docassemble is external to the docker network
+    ports:
+      - target: 9009
+        published: 9100
+      - target: 9000
+        published: 9101
+    command: /bin/sh /usr/src/app/docker_integration_test.sh
+    env_file: .env.test
+    depends_on:
+      - "db"
+    volumes:
+      - type: bind
+        source: ./src/main/config
+        target: /tmp/tls_certs
+      # Uncomment this for debugging: you can get exact SOAP envelopes being
+      # sent by running wireshark locally
+      - type: bind
+        source: .
+        target: /tmp/secrets
+      - type: bind
+        source: target
+        target: /tmp/jacoco
+    deploy:
+      resources:
+        limits:
+          memory: 4G
+  db:
+    image: "postgres:14"
+    shm_size: 128MB
+    env_file: .env.test
+    volumes:
+      - data-volume:/var/lib/postgresql/data
+
+volumes:
+  data-volume:

docker_integration_test.sh
@@ -1,0 +1,20 @@
+#! /bin/sh
+
+set -x
+cd /usr/src/app
+java \
+    -javaagent:jacocoagent.jar=destfile=/tmp/jacoco/jacoco.exec \
+    -cp $(cat cp.txt):target/efspserver.jar \
+    edu.suffolk.litlab.efspserver.db.LoginDatabase integrationTest true true > /tmp/secrets/proxy_stuff.txt
+# Add this before the `-cp` line if needed to get exact SOAP envelopes being sent
+# -javaagent:extract-tls-secrets-4.0.0.jar=/tmp/secrets/secrets.log \
+# Add this when we figure out ThreadPools and Unmarshalling (see #111)
+# -Djava.util.concurrent.ForkJoinPool.common.threadFactory=edu.suffolk.litlab.efspserver.JAXBForkJoinWorkerThreadFactory \
+# if needed to debug http logs:
+# https://stackoverflow.com/questions/53215038/how-to-log-request-response-using-java-net-http-httpclient
+exec java \
+    -javaagent:extract-tls-secrets-4.0.0.jar=/tmp/secrets/secrets.log \
+    -javaagent:jacocoagent.jar=destfile=/tmp/jacoco/jacoco.exec \
+    -cp $(cat cp.txt):target/efspserver.jar \
+    -XX:+HeapDumpOnOutOfMemoryError \
+    edu.suffolk.litlab.efspserver.services.EfspServer

integration_test.py
@@ -1,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import requests
+from docassemble.EFSPIntegration.test import integration_test
+
+from plumbum import local, FG, BG
+
+if not os.path.exists("target"):
+  os.mkdir("target")
+
+docker_compose = local.cmd.docker['compose', '-f', 'docker-integration-test.yml']
+docker_compose['build'] & FG
+docker_compose['up', '-d'] & FG
+
+server_up = False
+while not server_up:
+  try:
+    r = requests.get("http://localhost:9100/")
+    if r.ok:
+      server_up = True
+  except:
+    pass
+  time.sleep(5)
+
+with open("proxy_stuff.txt") as f:
+  for line in f:
+    if line.startswith("New Api Key for"):
+      proxy_key = line.split(": ")[1]
+      break
+
+integration_test.main(base_url="http://localhost:9100/", api_key=proxy_key, user_email=os.getenv("TYLER_USER_EMAIL"), user_password=os.getenv("TYLER_USER_PASSWORD"))
+
+docker_compose['down', '--timeout', '60', '--rmi', 'local'] & FG