fc7f2911361e069cbe6ba5a27e49437e1a0e511a
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-09-07 13:58:28 -0400

    Update MA prod URL test to pass
    Also added part of the test to check that all of the URLs aren't broken.

--- src/test/java/edu/suffolk/litlab/efspserver/SoapClientChooserTest.java
+++ src/test/java/edu/suffolk/litlab/efspserver/SoapClientChooserTest.java
@@ -1,18 +1,31 @@
 package edu.suffolk.litlab.efspserver;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.junit.jupiter.api.Test;
 
 public class SoapClientChooserTest {
   
+  public void testJurisdictionEnvUrl(String urlExpected, String urlGenerated) throws IOException {
+    assertEquals(urlExpected, urlGenerated);
+    URL url = new URL(urlGenerated);
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    int resp = conn.getResponseCode();
+    assertTrue("The generated URL (" + urlGenerated + ") returns an invalid HTTP error code: " + Integer.toString(resp), resp < 400);
+  }
+
   @Test
-  public void testJurisdictionEnvUrls() {
-    assertEquals("https://texas-stage.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("texas", "stage"));
-    assertEquals("https://texas.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("texas", "prod"));
-    assertEquals("https://massachusetts-stage.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("massachusetts", "stage"));
-    assertEquals("https://massachusetts.tylerhost.net/", SoapClientChooser.getCodeEndpointRootUrl("massachusetts", "prod"));
-    assertEquals("https://illinois-stage.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("illinois", "stage"));
-    assertEquals("https://illinois.tylerhost.net/", SoapClientChooser.getCodeEndpointRootUrl("illinois", "prod"));
+  public void testJurisdictionEnvUrls() throws IOException {
+    testJurisdictionEnvUrl("https://texas-stage.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("texas", "stage"));
+    testJurisdictionEnvUrl("https://texas.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("texas", "prod"));
+    testJurisdictionEnvUrl("https://massachusetts-stage.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("massachusetts", "stage"));
+    testJurisdictionEnvUrl("https://massachusetts.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("massachusetts", "prod"));
+    testJurisdictionEnvUrl("https://illinois-stage.tylertech.cloud/", SoapClientChooser.getCodeEndpointRootUrl("illinois", "stage"));
+    testJurisdictionEnvUrl("https://illinois.tylerhost.net/", SoapClientChooser.getCodeEndpointRootUrl("illinois", "prod"));
   }
 }
