84d8c62fb12f3d5a7978acba0382194fabf8ee32
Stan Ng <codestronger@gmail.com>
2024-06-08 22:00:47 -0700

    Ask the user for the server name when generating new API keys. Add section in the docs about the psql settings page in Supabase. This is useful for restoring a Postgres backup from the Docker postgres.

--- fly_create_api_key.sh
+++ fly_create_api_key.sh
@@ -1,1 +1,3 @@
-fly console --debug --verbose --vm-size shared-cpu-2x --vm-memory 4096 --command 'mvn -f /usr/src/app/pom.xml exec:java@LoginDatabase -Dexec.args="localhostServer true true"'
+echo "Who will be using this API key? This value will be stored in the server_name field."
+read server_name
+fly console --debug --verbose --vm-size shared-cpu-2x --vm-memory 4096 --command "mvn -f /usr/src/app/pom.xml exec:java@LoginDatabase -Dexec.args=\"$server_name true true\""
