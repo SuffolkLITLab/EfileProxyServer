45e1ae620c6a3f264f1fde441a576e92ea90b438
Bryce Willey <bryce.willey@suffolk.edu>
2025-06-04 14:47:34 -0400

    Revert change made in #207, and the alpine change in #206.
    Installing and using AWS-cli in Jammy seems to be broken somehow, getting this error:
    
    >  fatal error: An error occurred (403) when calling the HeadObject operation: Forbidden
    
    I assume that it's a permission thing in S3 (not sure how it's acting different in a different image), but it's something I'll have to fix later. Revert to a working state.

--- fly_startup_script.sh
+++ fly_startup_script.sh
@@ -7,12 +7,12 @@ if test -n "$FLY_MACHINE_ID"; then
   echo "Running Fly.io startup checks..."
   if ! test -f "$PATH_TO_KEYSTORE"; then
     echo "Installing cert"
-    apt-get update && apt-get install -y awscli
+    apk add --no-cache aws-cli
     aws s3 cp "$S3_TO_KEYSTORE_CERT" .
   fi
   if ! test -f "client_sign.properties"; then
     echo "Installing client_sign.properties"
-    apt-get update && apt-get install -y awscli
+    apk add --no-cache aws-cli
     aws s3 cp "$S3_TO_CLIENT_SIGN_PROPERTIES" .
   fi
 fi
