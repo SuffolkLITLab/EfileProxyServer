c1ab22a9f37db5cba4e2bc181e5048ef020de19e
Bryce Willey <bryce.willey@suffolk.edu>
2025-06-04 14:37:29 -0400

    Remove "yes" prompt when installing awscli

--- fly_startup_script.sh
+++ fly_startup_script.sh
@@ -7,12 +7,12 @@ if test -n "$FLY_MACHINE_ID"; then
   echo "Running Fly.io startup checks..."
   if ! test -f "$PATH_TO_KEYSTORE"; then
     echo "Installing cert"
-    apt update && apt install awscli
+    apt-get update && apt-get install -y awscli
     aws s3 cp "$S3_TO_KEYSTORE_CERT" .
   fi
   if ! test -f "client_sign.properties"; then
     echo "Installing client_sign.properties"
-    apt update && apt install awscli
+    apt-get update && apt-get install -y awscli
     aws s3 cp "$S3_TO_CLIENT_SIGN_PROPERTIES" .
   fi
 fi
