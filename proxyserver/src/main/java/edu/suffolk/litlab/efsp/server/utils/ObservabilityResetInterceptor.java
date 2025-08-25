12d9b6ba8a5e6b670d63440415196c3a6466ca4f
Bryce Willey <bryce.willey@suffolk.edu>
2025-08-25 13:08:40 -0400

    Show return status to all calls where our logic runs
    Makes the switch over to JAX-RS compliant container filters, which are
    better because:
    
    * The CXF API is incomprehensible; very difficult to find the right random string
      to get from the message, and very little documentation in the code about what each
      entry contains, when it will be there. I tried implemening this change previously
      with the CXF API, but both the response code and path were null. idk why, and didn't
      want to spend ages trying to figure it out.
    * we can eventually switch the HTTP server over to Spring (if desired; would clean up
      a bit of stuff: related to https://github.com/SuffolkLITLab/EfileProxyServer/blob/main/TODO.md).
    
    https://stackoverflow.com/a/46726824/11416267

--- proxyserver/src/main/java/edu/suffolk/litlab/efsp/server/utils/ObservabilityResetInterceptor.java
+++ proxyserver/src/main/java/edu/suffolk/litlab/efsp/server/utils/ObservabilityResetInterceptor.java
@@ -1,20 +1,27 @@
 package edu.suffolk.litlab.efsp.server.utils;
 
-import org.apache.cxf.interceptor.AbstractOutDatabindingInterceptor;
-import org.apache.cxf.interceptor.Fault;
-import org.apache.cxf.message.Message;
-import org.apache.cxf.phase.Phase;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Provider
 /** Clears all MDC entries after our business logic finishes running. */
-public class ObservabilityResetInterceptor extends AbstractOutDatabindingInterceptor {
-
-  public ObservabilityResetInterceptor() {
-    // See https://cxf.apache.org/docs/interceptors.html for phase descriptions
-    super(Phase.POST_LOGICAL);
-  }
+public class ObservabilityResetInterceptor implements ContainerResponseFilter {
+  private static final Logger log = LoggerFactory.getLogger(ObservabilityResetInterceptor.class);
 
   @Override
-  public void handleMessage(Message message) throws Fault {
+  public void filter(ContainerRequestContext request, ContainerResponseContext response)
+      throws IOException {
+    log.info(
+        "Sending back status of {} in response to {}",
+        response.getStatus(),
+        request.getUriInfo().getPath());
     MDCWrappers.removeAllMDCs();
   }
 }
