dd27af5a7a382deaafb69e7588102e0d90052d0c
Bryce Willey <bryce.willey@suffolk.edu>
2025-09-02 10:57:27 -0400

    Better authentication unit tests

--- proxyserver/src/test/java/edu/suffolk/litlab/efsp/db/LoginDatabaseTest.java
+++ proxyserver/src/test/java/edu/suffolk/litlab/efsp/db/LoginDatabaseTest.java
@@ -1,17 +1,16 @@
 package edu.suffolk.litlab.efsp.db;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import edu.suffolk.litlab.efsp.db.model.NewTokens;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -27,13 +26,24 @@ public class LoginDatabaseTest {
 
   private LoginDatabase ld;
 
+  private static final String TYLER_TOKEN = "tylerTOKEN";
+  private static final String JEFFNET_TOKEN = "jeffNetToken123";
+  private Map<String, Function<JsonNode, Optional<Map<String, String>>>> okFunctions =
+      Map.of(
+          "tyler", (info) -> Optional.of(Map.of("tyler_token", TYLER_TOKEN, "tyler_id", "12345")),
+          "jeffnet", (info) -> Optional.of(Map.of("jeffnet_token", JEFFNET_TOKEN)));
+
   @Container
-  public PostgreSQLContainer<?> postgres =
+  public static PostgreSQLContainer<?> postgres =
       new PostgreSQLContainer<>(DockerImageName.parse(DatabaseVersionTest.POSTGRES_DOCKER_NAME));
 
-  @BeforeEach
-  public void setUp() throws SQLException, NoSuchAlgorithmException {
+  @BeforeAll
+  public static void dbSetup() {
     postgres.start();
+  }
+
+  @BeforeEach
+  public void setUp() throws SQLException {
     Connection conn =
         DatabaseCreator.makeSingleConnection(
             postgres.getDatabaseName(),
@@ -41,6 +51,7 @@ public class LoginDatabaseTest {
             postgres.getUsername(),
             postgres.getPassword());
     ld = new LoginDatabase(conn);
+    ld.createTablesIfAbsent();
   }
 
   @AfterEach
@@ -49,32 +60,47 @@ public class LoginDatabaseTest {
   }
 
   @Test
-  public void testFromNothing() throws SQLException {
-    ld.createTablesIfAbsent();
+  public void testNoOp() throws SQLException {
     String cantDoAnything = ld.addNewUser("cantDoAnything", false, false);
-    String tylerOnly = ld.addNewUser("tylerOnly", true, false);
-    String everything = ld.addNewUser("everything", true, true);
-    Map<String, Function<JsonNode, Optional<Map<String, String>>>> okFunctions =
-        Map.of(
-            "tyler",
-                (info) -> Optional.of(Map.of("tyler_token", "tylerTOKEN", "tyler_id", "12345")),
-            "jeffnet", (info) -> Optional.of(Map.of("jeffnet_token", "jeffNetToken123")));
 
-    assertTrue(ld.login("fakeKey", "", okFunctions).isEmpty());
+    assertThat(ld.login("fakeKey", "", okFunctions)).isEmpty();
     // Do nothing should still succeed login (they can ping our codes API)
     var doNothing = ld.login(cantDoAnything, "{}", okFunctions);
-    assertTrue(doNothing.isPresent());
-    assertTrue(doNothing.get().getTokens().isEmpty());
+    assertThat(doNothing).isPresent();
+    assertThat(doNothing.get().getTokens()).isEmpty();
+
+    var inactiveTyler = ld.login(cantDoAnything, "{\"tyler\": {}}", okFunctions);
+    assertThat(inactiveTyler).isEmpty();
+  }
+
+  @Test
+  public void testTylerLogin() throws SQLException {
+    String tylerOnly = ld.addNewUser("tylerOnly", true, false);
 
     log.info("TylerOnly key: {}", tylerOnly);
     Optional<NewTokens> activeTyler = ld.login(tylerOnly, "{\"tyler\": {}}", okFunctions);
-    assertTrue(activeTyler.isPresent());
+    assertThat(activeTyler).isPresent();
     Optional<NewTokens> repeatLogin = ld.login(tylerOnly, "{\"tyler\": {}}", okFunctions);
-    assertTrue(repeatLogin.isPresent());
-    assertEquals(activeTyler.get(), repeatLogin.get());
+    assertThat(repeatLogin).isPresent();
+    assertThat(activeTyler.get()).isEqualTo(repeatLogin.get());
 
+    Optional<NewTokens> withJeffnet =
+        ld.login(tylerOnly, "{\"tyler\": {}, \"jeffnet\": {}}", okFunctions);
+    assertThat(withJeffnet).isEmpty();
+
+    Optional<NewTokens> withUnsupported =
+        ld.login(tylerOnly, "{\"tyler\": {}, \"unsupported\": {}}", okFunctions);
+    assertThat(withUnsupported).isEmpty();
+  }
+
+  @Test
+  public void testEverything() throws SQLException {
+    String everything = ld.addNewUser("everything", true, true);
     Optional<NewTokens> activeEverything =
         ld.login(everything, "{\"tyler\": {}, \"jeffnet\": {}}", okFunctions);
-    assertTrue(activeEverything.isPresent());
+    assertThat(activeEverything).isPresent();
+    var tokens = activeEverything.get().getTokens();
+    assertThat(tokens.get("tyler_token")).isEqualTo(TYLER_TOKEN);
+    assertThat(tokens.get("jeffnet_token")).isEqualTo(JEFFNET_TOKEN);
   }
 }
